2c7d35771691ea2ae7726ab3c11d965910d92f4e
Sergey Badulin <sbadulin@gmail.com>
2018-04-04 01:29:34 +0300

    set initial state to App component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const YOUTUBE_API_KEY = 'AIzaSyBnv0CSW4Oyo-Q2ocTUZgpxmmEdExgKdTE';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: YOUTUBE_API_KEY, term: 'surfboard' }, videos => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
